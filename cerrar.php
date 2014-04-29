0c145a699c6b0bd7cb3136e09196cf95840a6938
Nestor Eduardo <Shadow16@users.noreply.github.com>
2014-04-29 18:09:26 -0500

    Create cerrar.php

---
+++ cerrar.php
@@ -0,0 +1,17 @@
+<?php
+	SESSION_START();
+	SESSION_DESTROY();
+	header("location:sesion.php");
+?>
+<!--
+<html>
+  <head>
+    <title>Cerrar.php</title>
+  </head>
+<body>
+  <form method="POST" action="sesion.php">
+    <input type="submit" name="sesion" value="Ingresar">
+  </form>
+</body>
+</html>
+-->
